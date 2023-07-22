0961a8b597117f074006f9a3554f47df59956136
dzc0d3r <m4dcod3r@protonmail.com>
2023-07-23 00:07:48 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+
+
+
+
+function writeCards(names, message) {
+    let count = 0
+    let thankYou = []
+    while (names.length > count) {
+        thankYou[count] = `Thank you, ${names[count]}, for the wonderful ${message} gift!`
+        count++
+
+    }
+    return thankYou
+
+
+}
+
+
+
+function countDown(number) {
+    let count = 0
+    while (number >= count) {
+        console.log(number - count)
+        count++
+    }
+
+}
